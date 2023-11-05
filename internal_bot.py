edb0822bb18d2dca69964ea687e69589ca2984d2
Alejandro Sanchez <alejandross1811@gmail.com>
2023-11-05 12:19:15 -0500

    Feat: Open Ai text response AI implementation using text-davinci-003

--- internal_bot.py
+++ internal_bot.py
@@ -3,26 +3,28 @@
 import discord
 from discord.ext import commands
 from secret import *
+import openai
 
 # End Imports
 
 # Begin Definitions
-
-client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-intents = discord.Intents.default()
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix="!", intents=intents)
 intents.message_content = True
 intents.members = True
-TOKEN = open("token.txt","r").read()
+openai.api_key = token_chatgpt
 
 # End Definitions
 
-#Main Code Begin
+# Main Code Begin
 
 # Events Begin
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+
+## Join and Remove events
 
 @client.event
 async def on_member_join(member):
@@ -33,6 +35,25 @@ async def on_member_join(member):
 async def on_member_remove(member):
     channel = client.get_channel(1169097169970671737)
     await channel.send(f'{member} Just left the server, Press F')
+
+## Chat Bot event using Open AI
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+    
+    if client.user in message.mentions:
+
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"{message.content}",
+            max_tokens=2048,
+            temperature=0.5,
+        )
+
+        await message.channel.send(response.choices[0].text)
 
 # Events End
 
